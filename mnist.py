78baf1074c83214eae09ebeb27c1aea4bb084384
Sameer Gauria <sameergauria@yahoo.com>
2012-12-27 16:07:35 -0500

    Parse input.
    Can at least parse input files now. No real work done yet.

--- mnist.py
+++ mnist.py
@@ -1,32 +1,76 @@
 
 training  = 0
-imgfile   = "train-images-idx3-ubyte" if training else "t10k-images-idx3-ubyte"
+imagefile = "train-images-idx3-ubyte" if training else "t10k-images-idx3-ubyte"
 labelfile = "train-labels-idx1-ubyte" if training else "t10k-labels-idx1-ubyte"
 
-def bytelist_to_int(bytelist):
+def bytelist_to_uint(bytelist):
     val = 0
     for b in bytelist :
         val = val * 256 + b
     return val
 
-def read_img_file(filename):
+def read_idx_file(filename):
     with open(filename, 'rb') as f:
         bytelist = f.read()
-        assert(bytelist[0:4] == bytes([0,0,8,3])) # Magic value.
-        num_imgs = bytelist_to_int(bytelist[ 4: 8])
-        num_rows = bytelist_to_int(bytelist[ 8:12]) # Is this row or col ?
-        num_cols = bytelist_to_int(bytelist[12:16])
-        print(num_imgs)        
-        print(num_rows)        
-        print(num_cols)        
+        assert(bytelist[0:2] == bytes([0,0])) # Magic value.
+        datatype = bytelist[2]
+        num_dims = bytelist[3]
+        num_imgs = bytelist_to_uint(bytelist[ 4: 8])
+        start_pos = 8
+        dim_sizes = [num_imgs]
+        for i in range(num_dims - 1):
+            dim_sizes.append( bytelist_to_uint(bytelist[8+4*i:12+4*i]) )
+            start_pos += 4
+            
+        print(num_dims)
+        print(dim_sizes)
 
-def read_label_file(filename):
-    with open(filename, 'rb') as f:
-        bytelist = f.read()
-        assert(bytelist[0:4] == bytes([0,0,8,1])) # Magic value.
-        num_imgs = bytelist_to_int(bytelist[ 4: 8])
-        print(num_imgs)        
+        results, bytes_read = get_one_idx_entry(bytelist, start_pos, num_dims, dim_sizes, datatype)
+        assert (start_pos + bytes_read == len(bytelist)) # Whole list consumed.
+
+        return results       
+
+def get_one_idx_entry(bytelist, start_pos, num_dims, dim_sizes, datatype):
+        assert(datatype == 8) # Only support unsigned bytes right now.
+        datasize = 1
+        if num_dims == 0:
+            v = bytelist_to_uint(bytelist[start_pos:start_pos+datasize])
+            #print (start_pos, v)
+            return v, datasize
+        else :
+            results = []
+            sp = start_pos
+            for i in range(dim_sizes[0]):
+                r, sp_inc = get_one_idx_entry(bytelist, sp, num_dims-1, dim_sizes[1:], datatype)
+                results.append(r)
+                sp += sp_inc
+            return results, sp - start_pos
+                
+
+def image_to_str(img):
+    """ Assumes img is a 2 array with values in range 0 - 255 """
+    s = ""
+    for y in range(len(img)):
+        row = img[y]
+        for x in range(len(row)):
+            p = row[x]
+            s += val_to_char(p)
+        s += "\n"
+
+    return s
+
+def val_to_char(p):
+    c = ' '
+    if p > 128:
+        c = '#'
+    return c
 
 
-read_img_file(imgfile)
-    
+image_list = read_idx_file(imagefile)
+label_list = read_idx_file(labelfile)
+idx = 402
+i = image_list[idx]
+l = label_list[idx]
+
+print (l)
+print (image_to_str(i))
